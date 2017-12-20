1aea44aa2ff50ccf627393b16db1970ea150472f
surinderpalsinghsidhu <surindercoder@gmail.com>
2017-12-19 19:07:59 -0500

    2nd commit

--- app/src/main/java/com/example/surinderpalsinghsidhu/justjava/MainActivity.java
+++ app/src/main/java/com/example/surinderpalsinghsidhu/justjava/MainActivity.java
@@ -11,7 +11,7 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
-
+int quantity = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*5);
+
+        display(quantity);
+        displayPrice(quantity * 5 );
     }
 
     /**
@@ -40,5 +41,15 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    public void increment(View view){
+       quantity = quantity + 1;
+        display(quantity);
+
+    }
+    public void decrement(View view){
+        quantity = quantity - 1;
+        display(quantity);
+
     }
 }
